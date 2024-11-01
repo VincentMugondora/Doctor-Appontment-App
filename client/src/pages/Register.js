d4dca8190e008677af4947cb2db87af8ad02c7d2
VincentMugondora <vinmugondora@gmail.com>
2024-11-01 08:54:20 +0200

    "Added CSS reset and created Register form with Ant Design components"

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,11 +1,38 @@
-import React from 'react'
+import "../styles/RegisterStyles.css"
+import { Form, Input } from "antd";
 
 const Register = () => {
-  return (
-    <div>
-      <h1>Register page</h1>
-    </div>
-  )
-}
+  const onFinishHandler = (values) => {
+    console.log(values);
+  };
 
-export default Register
+  return (
+    <>
+      <div className="form-container">
+        <Form layout="vertical" onFinish={onFinishHandler}>
+          <h1>Register Form</h1>
+
+          {/* name input */}
+          <Form.Item label="Name" name="name">
+            <Input type="text" required />
+          </Form.Item>
+
+          {/* email */}
+          <Form.Item label="Email" name="email">
+            <Input type="email" required />
+          </Form.Item>
+
+          {/* password */}
+          <Form.Item label="Password" name="password">
+            <Input type="password" required />
+          </Form.Item>
+
+          {/* button */}
+          <button className="btn btn-primary" type="submit">Register</button>
+        </Form>
+      </div>
+    </>
+  );
+};
+
+export default Register;
